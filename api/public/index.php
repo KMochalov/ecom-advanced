9d7b43dad1d8df84e2e24396eef00fe3ed32fe95
Konstantin Mochalov <mochalov.ka@sibvaleo.com>
2024-01-02 16:33:01 +0300

    Установил контейнер

--- api/public/index.php
+++ api/public/index.php
@@ -6,11 +6,28 @@ use Slim\Factory\AppFactory;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+http_response_code(500);
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new DI\ContainerBuilder();
+
+$builder->addDefinitions(
+    [
+        'config' => [
+            'debug' =>  (bool) getenv('APP_DEBUG')
+        ]
+    ]
+);
+
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
+    throw new DomainException();
     $response->getBody()->write('{"test":"test"}');
     return $response->withHeader('Content-Type','application/json');
 });
