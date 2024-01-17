eaad48169a4246821d36e783364576720ba7587a
Konstantin Mochalov <mochalov.ka@sibvaleo.com>
2024-01-17 15:19:46 +0300

    Начал модуль аунтентификации

---
+++ api/src/Auth/RegisterByEmail/Request/Handler.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\RegisterByEmail\Request;
+
+use App\Auth\RegisterByEmail\Request\Command;
+
+class Handler
+{
+    public function handler(Command $command): void
+    {
+
+    }
+}
