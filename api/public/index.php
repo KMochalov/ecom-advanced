4fc0fac9e8e52e6b30eaa4aca7a1bbba121e57d5
Konstantin Mochalov <mochalov.ka@sibvaleo.com>
2024-03-17 15:52:31 +0300

    Переехали на симфони

--- api/public/index.php
+++ api/public/index.php
@@ -1,19 +1,9 @@
 <?php
 
-declare(strict_types=1);
+use App\Kernel;
 
-use Psr\Container\ContainerInterface;
-use Slim\App;
-use Slim\Factory\AppFactory;
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-http_response_code(500);
-
-require __DIR__ . '/../vendor/autoload.php';
-
-/** @var ContainerInterface $container */
-$container = require_once __DIR__ . '/../config/container.php';
-
-/** @var App $app */
-$app = (require_once __DIR__ . '/../config/app_bootstrap.php')($container);
-
-$app->run();
+return function (array $context) {
+    return new Kernel((string) $context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
