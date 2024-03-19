9a19ca4474f4c1f407ed8933ce4d86fc2c035ca1
Alcoweb <alcowebdev@gmail.com>
2024-03-19 17:35:36 +0300

    Добавил Роли

--- api/src/Auth/Command/Role/Handler.php
+++ api/src/Auth/Command/Role/Handler.php
@@ -4,15 +4,8 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\Role;
 
-use App\Auth\Entity\User\Email;
 use App\Auth\Entity\User\Role;
-use App\Auth\Entity\User\User;
 use App\Auth\Repository\UserRepositoryInterface;
-use App\Auth\Serivces\HasherInterface;
-use App\Auth\Serivces\SenderInterface;
-use App\Auth\Serivces\TokenizerInterface;
-use DateTimeImmutable;
-use DomainException;
 use App\Auth\Entity\User\Id;
 use App\Utils\Flusher;
 
@@ -28,9 +21,7 @@ class Handler
     public function handle(Command $command): void
     {
         $user = $this->repository->get(new Id($command->userId));
-
         $role = new Role($command->role);
-
         $user->changeRole($role);
 
         $this->flusher->flush();
