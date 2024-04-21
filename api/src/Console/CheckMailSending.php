f56819fc84bfeb30a514dd3f7b48aee29677fcb1
Alcoweb <alcowebdev@gmail.com>
2024-04-21 20:08:28 +0300

    установил мэйлхог и добавил простую комманду для проверки отправки писем.

---
+++ api/src/Console/CheckMailSending.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class CheckMailSending extends Command
+{
+    public function __construct(private MailerInterface $mailer, ?string $name = null)
+    {
+        parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('mail:check');
+        $this->setDescription('Check Email Sending');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->write('<info>Hello</info>');
+
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $this->mailer->send($email);
+
+       return 1;
+    }
+}
